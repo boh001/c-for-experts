dbfc61e052a42294e1fcaa034284a36e6a0e8551
camel <boh001g@gmail.com>
2022-12-29 21:21:14 +0900

    [ADD] Add chatper

---
+++ chapter4/ExtremeC_examples_chapter4_2.c
@@ -0,0 +1,7 @@
+int global_var1;
+int global_var2;
+int global_var3;
+
+int main() {
+	return 0;
+}
---
+++ chapter4/ExtremeC_examples_chapter4_4.c
@@ -0,0 +1,7 @@
+int x = 33;
+int y = 0x12143476;
+char z[6] = "ABCDE";
+
+int main() {
+	return 0;
+}
